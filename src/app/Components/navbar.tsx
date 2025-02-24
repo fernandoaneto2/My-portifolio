70124fa5d886ed9dce8cec07b99d2f4e51184e62
fernandoaneto2 <fernando.neto02@gmail.com>
2025-02-24 10:47:18 -0300

    chore: Atualização do navbar

--- src/app/Components/navbar.tsx
+++ src/app/Components/navbar.tsx
@@ -1,37 +1,15 @@
-// import React from "react";
-// import { Button } from "@/components/ui/button";
-// import Link from "next/link";
-
-// interface NavBarProps {
-//   href: string;
-//   nome: React.ReactNode;
-// }
-
-// const OptionsNavBar = ({ href, nome }: NavBarProps) => {
-//   return (
-//     <Link href={href}>
-//       <Button>{nome}</Button>
-//     </Link>
-//   );
-// };
-
-// export default OptionsNavBar;
 "use client";
 import Link from "next/link";
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
 import FotoDePerfil from "../../../public/fotoperfil.png";
-import { useState } from "react";
 import React from "react";
 
 export default function NavBar() {
-  const [isOpen, setIsOpen] = useState(false);
-
-  const toggleMenu = () => setIsOpen(!isOpen);
-
   return (
-    <div className="flex md:flex-row p-2 justify-evenly box-border">
-      <div className="w-10 h-10 md:w-16 md:h-16 flex-shrink-0">
+    <div className="flex flex-row justify-between p-4 bg-zinc-800">
+      {/* Seção de imagem */}
+      <div className=" md:w-16 md:h-16 flex-shrink-0">
         <Link href="/">
           <Image
             className="rounded-full"
@@ -44,36 +22,86 @@ export default function NavBar() {
         </Link>
       </div>
 
-      {/* Botão hamburger */}
-      <button
-        className="md:hidden flex flex-col space-y-1 absolute top-4 right-4 z-10"
-        onClick={toggleMenu}
-      >
-        <div className="w-6 h-1 bg-black"></div>
-        <div className="w-6 h-1 bg-black"></div>
-        <div className="w-6 h-1 bg-black"></div>
-      </button>
-
       {/* Menu de navegação */}
-      <div
-        className={`flex flex-col md:flex-row gap-4 items-center mt-4 md:mt-0 ${
-          isOpen ? "block" : "hidden"
-        } md:block w-full md:w-auto md:gap-6`}
-      >
+      <div className={"flex gap-4 items-center flex-shrink-0"}>
         <Link href="/pages/about">
-          <Button>Sobre</Button>
+          <button
+            type="submit"
+            className="flex justify-center gap-2 items-center mx-auto shadow-xl text-lg bg-gray-50 backdrop-blur-md lg:font-semibold isolation-auto border-gray-50 before:absolute before:w-full before:transition-all before:duration-700 before:hover:w-full before:-left-full before:hover:left-0 before:rounded-full before:bg-emerald-500 hover:text-gray-50 before:-z-10 before:aspect-square before:hover:scale-150 before:hover:duration-700 relative z-10 px-4 py-2 overflow-hidden border-2 rounded-full group"
+          >
+            {" "}
+            About
+            {/* <svg
+              className="w-8 h-8 justify-end group-hover:rotate-90 group-hover:bg-gray-50 text-gray-50 ease-linear duration-300 rounded-full border border-gray-700 group-hover:border-none p-2 rotate-45"
+              viewBox="0 0 16 19"
+              xmlns="http://www.w3.org/2000/svg"
+            > */}
+            <path
+              d="M7 18C7 18.5523 7.44772 19 8 19C8.55228 19 9 18.5523 9 18H7ZM8.70711 0.292893C8.31658 -0.0976311 7.68342 -0.0976311 7.29289 0.292893L0.928932 6.65685C0.538408 7.04738 0.538408 7.68054 0.928932 8.07107C1.31946 8.46159 1.95262 8.46159 2.34315 8.07107L8 2.41421L13.6569 8.07107C14.0474 8.46159 14.6805 8.46159 15.0711 8.07107C15.4616 7.68054 15.4616 7.04738 15.0711 6.65685L8.70711 0.292893ZM9 18L9 1H7L7 18H9Z"
+              className="fill-gray-800 group-hover:fill-gray-800"
+            ></path>
+            {/* </svg> */}
+          </button>
         </Link>
 
         <Link href="/pages/contact">
-          <Button>Contact</Button>
+          <button
+            type="submit"
+            className="flex justify-center gap-2 items-center mx-auto shadow-xl text-lg bg-gray-50 backdrop-blur-md lg:font-semibold isolation-auto border-gray-50 before:absolute before:w-full before:transition-all before:duration-700 before:hover:w-full before:-left-full before:hover:left-0 before:rounded-full before:bg-emerald-500 hover:text-gray-50 before:-z-10 before:aspect-square before:hover:scale-150 before:hover:duration-700 relative z-10 px-4 py-2 overflow-hidden border-2 rounded-full group"
+          >
+            {" "}
+            Contact
+            {/* <svg
+              className="w-8 h-8 justify-end group-hover:rotate-90 group-hover:bg-gray-50 text-gray-50 ease-linear duration-300 rounded-full border border-gray-700 group-hover:border-none p-2 rotate-45"
+              viewBox="0 0 16 19"
+              xmlns="http://www.w3.org/2000/svg"
+            > */}
+            <path
+              d="M7 18C7 18.5523 7.44772 19 8 19C8.55228 19 9 18.5523 9 18H7ZM8.70711 0.292893C8.31658 -0.0976311 7.68342 -0.0976311 7.29289 0.292893L0.928932 6.65685C0.538408 7.04738 0.538408 7.68054 0.928932 8.07107C1.31946 8.46159 1.95262 8.46159 2.34315 8.07107L8 2.41421L13.6569 8.07107C14.0474 8.46159 14.6805 8.46159 15.0711 8.07107C15.4616 7.68054 15.4616 7.04738 15.0711 6.65685L8.70711 0.292893ZM9 18L9 1H7L7 18H9Z"
+              className="fill-gray-800 group-hover:fill-gray-800"
+            ></path>
+            {/* </svg> */}
+          </button>
         </Link>
 
         <Link href="/pages/projects">
-          <Button>Projects</Button>
+          <button
+            type="submit"
+            className="flex justify-center gap-2 items-center mx-auto shadow-xl text-lg bg-gray-50 backdrop-blur-md lg:font-semibold isolation-auto border-gray-50 before:absolute before:w-full before:transition-all before:duration-700 before:hover:w-full before:-left-full before:hover:left-0 before:rounded-full before:bg-emerald-500 hover:text-gray-50 before:-z-10 before:aspect-square before:hover:scale-150 before:hover:duration-700 relative z-10 px-4 py-2 overflow-hidden border-2 rounded-full group"
+          >
+            {" "}
+            Projects
+            {/* <svg
+              className="w-8 h-8 justify-end group-hover:rotate-90 group-hover:bg-gray-50 text-gray-50 ease-linear duration-300 rounded-full border border-gray-700 group-hover:border-none p-2 rotate-45"
+              viewBox="0 0 16 19"
+              xmlns="http://www.w3.org/2000/svg"
+            > */}
+            <path
+              d="M7 18C7 18.5523 7.44772 19 8 19C8.55228 19 9 18.5523 9 18H7ZM8.70711 0.292893C8.31658 -0.0976311 7.68342 -0.0976311 7.29289 0.292893L0.928932 6.65685C0.538408 7.04738 0.538408 7.68054 0.928932 8.07107C1.31946 8.46159 1.95262 8.46159 2.34315 8.07107L8 2.41421L13.6569 8.07107C14.0474 8.46159 14.6805 8.46159 15.0711 8.07107C15.4616 7.68054 15.4616 7.04738 15.0711 6.65685L8.70711 0.292893ZM9 18L9 1H7L7 18H9Z"
+              className="fill-gray-800 group-hover:fill-gray-800"
+            ></path>
+            {/* </svg> */}
+          </button>
         </Link>
 
         <Link href="/pages/techs">
-          <Button>Techs</Button>
+          <button
+            type="submit"
+            className="flex justify-center gap-2 items-center mx-auto shadow-xl text-lg bg-gray-50 backdrop-blur-md lg:font-semibold isolation-auto border-gray-50 before:absolute before:w-full before:transition-all before:duration-700 before:hover:w-full before:-left-full before:hover:left-0 before:rounded-full before:bg-emerald-500 hover:text-gray-50 before:-z-10 before:aspect-square before:hover:scale-150 before:hover:duration-700 relative z-10 px-4 py-2 overflow-hidden border-2 rounded-full group"
+          >
+            {" "}
+            Techs
+            {/* <svg
+              className="w-8 h-8 justify-end group-hover:rotate-90 group-hover:bg-gray-50 text-gray-50 ease-linear duration-300 rounded-full border border-gray-700 group-hover:border-none p-2 rotate-45"
+              viewBox="0 0 16 19"
+              xmlns="http://www.w3.org/2000/svg"
+            > */}
+            <path
+              d="M7 18C7 18.5523 7.44772 19 8 19C8.55228 19 9 18.5523 9 18H7ZM8.70711 0.292893C8.31658 -0.0976311 7.68342 -0.0976311 7.29289 0.292893L0.928932 6.65685C0.538408 7.04738 0.538408 7.68054 0.928932 8.07107C1.31946 8.46159 1.95262 8.46159 2.34315 8.07107L8 2.41421L13.6569 8.07107C14.0474 8.46159 14.6805 8.46159 15.0711 8.07107C15.4616 7.68054 15.4616 7.04738 15.0711 6.65685L8.70711 0.292893ZM9 18L9 1H7L7 18H9Z"
+              className="fill-gray-800 group-hover:fill-gray-800"
+            ></path>
+            {/* </svg> */}
+          </button>
         </Link>
       </div>
     </div>
